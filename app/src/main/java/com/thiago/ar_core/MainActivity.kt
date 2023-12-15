343a4d16a3f17cccd754dd95837259defea36fa4
unknown <thiagocostadejesus2@gmail.com>
2023-12-15 20:26:40 -0300

    first commit

---
+++ app/src/main/java/com/thiago/ar_core/MainActivity.kt
@@ -0,0 +1,176 @@
+package com.thiago.ar_core
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.google.ar.core.Config
+import com.thiago.ar_core.ui.theme.Ar_CoreTheme
+import com.thiago.ar_core.ui.theme.Translucent
+import io.github.sceneview.ar.ARScene
+import io.github.sceneview.ar.node.ArModelNode
+import io.github.sceneview.ar.node.ArNode
+import io.github.sceneview.ar.node.PlacementMode
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Ar_CoreTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Box(modifier = Modifier.fillMaxSize()) {
+                        val currentModel = remember {
+                            mutableStateOf("")
+                        }
+                        ARScreen(currentModel.value)
+                        Menu(modifier = Modifier.align(Alignment.BottomCenter)) {
+                            currentModel.value = it
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+    @Composable
+    fun Menu(modifier: Modifier,onClick:(String)->Unit) {
+        var currentIndex by remember {
+            mutableStateOf(0)
+        }
+
+    val itemsList = listOf(
+        Food("brahma",R.drawable.brahma),
+//        Food("instant",R.drawable.instant),
+//        Food("momos",R.drawable.momos),
+//        Food("pizza",R.drawable.pizza),
+//        Food("ramen",R.drawable.ramen),
+
+        )
+    fun updateIndex(offset:Int){
+        currentIndex = (currentIndex+offset + itemsList.size) % itemsList.size
+        onClick(itemsList[currentIndex].name)
+    }
+    Row(modifier = modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceAround
+    ) {
+        IconButton(onClick = {
+            updateIndex(-1)
+        }) {
+            Icon(painter = painterResource(id = R.drawable.ic_arrow_back), contentDescription ="previous" )
+        }
+
+        CircularImage(imageId = itemsList[currentIndex].imageId )
+
+        IconButton(onClick = {
+            updateIndex(1)
+        }) {
+            Icon(painter = painterResource(id = R.drawable.ic_arrow_forward), contentDescription ="next")
+        }
+    }
+
+}
+
+@Composable
+fun CircularImage(
+    modifier: Modifier=Modifier,
+    imageId: Int
+) {
+    Box(modifier = modifier
+        .size(140.dp)
+        .clip(CircleShape)
+        .border(width = 3.dp, Translucent, CircleShape)
+    ){
+        Image(painter = painterResource(id = imageId), contentDescription = null, modifier = Modifier.size(140.dp), contentScale = ContentScale.FillBounds)
+    }
+}
+
+@Composable
+fun ARScreen(model:String) {
+    val nodes = remember {
+        mutableListOf<ArNode>()
+    }
+    val modelNode = remember {
+        mutableStateOf<ArModelNode?>(null)
+    }
+    val placeModelButton = remember {
+        mutableStateOf(false)
+    }
+    Box(modifier = Modifier.fillMaxSize()){
+        ARScene(
+            modifier = Modifier.fillMaxSize(),
+            nodes = nodes,
+            planeRenderer = true,
+            onCreate = {arSceneView ->
+                arSceneView.lightEstimationMode = Config.LightEstimationMode.DISABLED
+                arSceneView.planeRenderer.isShadowReceiver = false
+                modelNode.value = ArModelNode(arSceneView.engine,PlacementMode.INSTANT).apply {
+                    loadModelGlbAsync(
+                        glbFileLocation = "models/${model}.glb",
+                        scaleToUnits = 0.8f
+                    ){
+
+                    }
+                    onAnchorChanged = {
+                        placeModelButton.value = !isAnchored
+                    }
+                    onHitResult = {node, hitResult ->
+                        placeModelButton.value = node.isTracking
+                    }
+
+                }
+                nodes.add(modelNode.value!!)
+            },
+            onSessionCreate = {
+                planeRenderer.isVisible = false
+            }
+        )
+        if(placeModelButton.value){
+            Button(onClick = {
+                modelNode.value?.anchor()
+            }, modifier = Modifier.align(Alignment.Center)) {
+                Text(text = "Place It")
+            }
+        }
+    }
+
+    LaunchedEffect(key1 = model){
+        modelNode.value?.loadModelGlbAsync(
+            glbFileLocation = "models/${model}.glb",
+            scaleToUnits = 0.8f
+        )
+
+    }
+
+}
+
+    data class Food(var name:String,var imageId:Int)
+
+
+
+
